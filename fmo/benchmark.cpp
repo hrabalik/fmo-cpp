6e43335ac5eee0929e66757e594f4595cbbee082
Aleš Hrabalík <tufak064@gmail.com>
2017-02-20 15:21:32 +0100

    Add more benchmarks

--- fmo/benchmark.cpp
+++ fmo/benchmark.cpp
@@ -33,6 +33,7 @@ namespace fmo {
 
         try {
             log(logFunc, "Benchmark started.\n");
+            log(logFunc, "Num threads: %d\n", cv::getNumThreads());
 
             for (auto func : mFuncs) {
                 stats.reset();
@@ -62,10 +63,17 @@ namespace fmo {
     namespace {
         struct {
             cv::Mat grayNoise;
-            cv::Mat grayCheckerboard;
+            cv::Mat grayCircles;
+            cv::Mat rect;
+
             cv::Mat out1;
             cv::Mat out2;
             cv::Mat out3;
+
+            std::mt19937 re{5489};
+            using limits = std::numeric_limits<int>;
+            std::uniform_int_distribution<int> uniform{limits::min(), limits::max()};
+            std::uniform_int_distribution<int> randomGray{2, 254};
         } global;
 
         struct Init {
@@ -75,44 +83,43 @@ namespace fmo {
             static cv::Mat newGrayMat() { return {cv::Size{W, H}, CV_8UC1}; }
 
             Init() {
-                std::mt19937 re{5489};
-                using limits = std::numeric_limits<int>;
-                std::uniform_int_distribution<int> uniform{limits::min(), limits::max()};
 
                 {
                     global.grayNoise = newGrayMat();
                     auto* data = global.grayNoise.data;
                     auto* end = data + (W * H);
 
-                    for (; data < end; data += sizeof(int)) { *(int*)data = uniform(re); }
+                    for (; data < end; data += sizeof(int)) {
+                        *(int*)data = global.uniform(global.re);
+                    }
 
                     // cv::imwrite("grayNoise.png", global.grayNoise);
                 }
 
                 {
-                    global.grayCheckerboard = newGrayMat();
-                    auto* data = global.grayCheckerboard.data;
+                    global.grayCircles = newGrayMat();
+                    auto* data = global.grayCircles.data;
 
                     for (int r = 0; r < H; r++) {
-                        bool a = ((r * r) % 100000) > 50000;
+                        int rmod = ((r + 128) % 256);
+                        int dy = std::min(rmod, 256 - rmod);
+                        int dy2 = dy * dy;
                         for (int c = 0; c < W; c++) {
-                            bool b = (c % 512) > 256;
-                            *data++ = (a ^ b) ? 0xFF : 0x00;
+                            int cmod = ((c + 128) % 256);
+                            int dx = std::min(cmod, 256 - cmod);
+                            int dx2 = dx * dx;
+                            *data++ = (dx2 + dy2 < 10000) ? 0xFF : 0x00;
                         }
                     }
 
-                    // cv::imwrite("grayCheckerboard.png", global.grayCheckerboard);
+                    // cv::imwrite("grayCircles.png", global.grayCircles);
                 }
+
+                global.rect = cv::getStructuringElement(cv::MORPH_RECT, {3, 3});
             }
         };
 
         void init() { static Init once; }
-
-        Benchmark FMO_UNIQUE_NAME{"cv::absdiff", []() {
-                                      init();
-                                      cv::absdiff(global.grayNoise, global.grayCheckerboard,
-                                                  global.out1);
-                                  }};
 
         Benchmark FMO_UNIQUE_NAME{"cv::threshold", []() {
                                       init();
@@ -120,9 +127,36 @@ namespace fmo {
                                                     cv::THRESH_BINARY);
                                   }};
 
+        Benchmark FMO_UNIQUE_NAME{"cv::absdiff", []() {
+                                      init();
+                                      cv::absdiff(global.grayNoise, global.grayCircles,
+                                                  global.out1);
+                                  }};
+
+        Benchmark FMO_UNIQUE_NAME{"cv::dilate", []() {
+                                      init();
+                                      cv::dilate(global.grayNoise, global.out1, global.rect);
+                                  }};
+
+        Benchmark FMO_UNIQUE_NAME{"cv::erode", []() {
+                                      init();
+                                      cv::erode(global.grayNoise, global.out1, global.rect);
+                                  }};
+
+        Benchmark FMO_UNIQUE_NAME{"cv::floodFill", []() {
+                                      init();
+                                      auto newVal = uchar(global.randomGray(global.re));
+                                      cv::floodFill(global.grayCircles, cv::Point{0, 0}, newVal);
+                                  }};
+
         Benchmark FMO_UNIQUE_NAME{"cv::connectedComponents", []() {
                                       init();
-                                      cv::connectedComponentsWithStats(global.grayCheckerboard,
+                                      cv::connectedComponents(global.grayCircles, global.out1);
+                                  }};
+
+        Benchmark FMO_UNIQUE_NAME{"cv::connectedComponentsWithStats", []() {
+                                      init();
+                                      cv::connectedComponentsWithStats(global.grayCircles,
                                                                        global.out1, global.out2,
                                                                        global.out3);
                                   }};
