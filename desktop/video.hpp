2431aa46fc191a8e549dad74d1614abc3c4ca3df
Aleš Hrabalík <tufak064@gmail.com>
2017-03-09 19:18:34 +0100

    Add documentation for VideoInput, VideoOutput

--- desktop/video.hpp
+++ desktop/video.hpp
@@ -12,21 +12,27 @@ namespace cv {
     class VideoWriter;
 }
 
+/// For reading image data from a video file or a camera device.
 struct VideoInput {
     VideoInput() = delete;
     VideoInput(const VideoInput&) = delete;
     VideoInput& operator=(const VideoInput&) = delete;
     VideoInput(VideoInput&&);
     VideoInput& operator=(VideoInput&&);
-
     VideoInput(std::unique_ptr<cv::VideoCapture>&& cap);
 
+    /// Opens a camera device for reading. Throws on error.
     static std::unique_ptr<VideoInput> makeFromCamera(int camId);
+
+    /// Opens a video file for reading. Throws on error.
     static std::unique_ptr<VideoInput> makeFromFile(const std::string& filename);
+
+    /// Provides the next frame from the file or device. May block until the next frame is
+    /// available. If there are no more frames, the returned region will point to nullptr.
+    fmo::Region receiveFrame();
 
     fmo::Dims dims() const { return mDims; }
     float fps() const { return mFps; }
-    fmo::Region receiveFrame();
 
 private:
     // data
@@ -36,20 +42,25 @@ private:
     float mFps;
 };
 
+/// For writing data into a video file.
 struct VideoOutput {
     VideoOutput() = delete;
     VideoOutput(const VideoOutput&) = delete;
     VideoOutput& operator=(const VideoOutput&) = delete;
     VideoOutput(VideoOutput&&);
     VideoOutput& operator=(VideoOutput&&);
-
     VideoOutput(std::unique_ptr<cv::VideoWriter>&& writer, fmo::Dims dims);
 
+    /// Creates a new video file. Throws on error.
     static std::unique_ptr<VideoOutput> makeFile(const std::string& filename, fmo::Dims dims,
                                                  float fps);
+
+    /// Creates a new file in the specified directory. The directory must exist. The file name is
+    /// generated based on current system time with second precision. Throws on error.
     static std::unique_ptr<VideoOutput> makeInDirectory(const std::string& dir, fmo::Dims dims,
                                                         float fps);
 
+    /// Saves a frame to the video file.
     void sendFrame(const fmo::Mat& frame);
 
 private:
