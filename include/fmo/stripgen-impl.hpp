d2f1c3173c915f75c18db12e59a83de384890367
Aleš Hrabalík <tufak064@gmail.com>
2017-04-05 23:05:46 +0200

    Process multiple columns at a time

--- include/fmo/stripgen-impl.hpp
+++ include/fmo/stripgen-impl.hpp
@@ -2,72 +2,95 @@
 #define FMO_STRIPGEN_IMPL_HPP
 
 #include <algorithm>
+#include <fmo/assert.hpp>
 #include <fmo/stripgen.hpp>
 
 namespace fmo {
     template <typename CallbackFunc>
     void StripGen::operator()(const fmo::Mat& img, int minHeight, int minGap, int step,
                               CallbackFunc cb) {
+        using batch_t = uint32_t;
+        constexpr int WIDTH = int(sizeof(batch_t) / sizeof(uint8_t));
+        FMO_ASSERT(int(img.skip()) % WIDTH == 0, "StripGen::operator(): bad skip");
+
         const Dims dims = img.dims();
-        const int skip = int(img.skip());
+        const int arrSz = dims.height + 4;
+        const int skip = int(img.skip()) / WIDTH;
         const int halfStep = step / 2;
         const int pad = std::max(0, std::max(minHeight, minGap));
-        const uint8_t* colData = img.data();
+        const batch_t* colData = (const batch_t*)(img.data());
         mNoise = 0;
         int16_t origX = int16_t(halfStep);
-        mRle.resize(dims.height + 4);
+        mRle.resize(arrSz * WIDTH);
 
-        for (int col = 0; col < dims.width; col++, colData++, origX += int16_t(step)) {
-            const uint8_t* data = colData;
-            int* front = mRle.data();
-            int* back = front;
-            int n = 0;
+        int* front[WIDTH];
+        for (int w = 0; w < WIDTH; w++) { front[w] = mRle.data() + (w * arrSz); }
 
-            // add top of image
-            *back = -pad;
-            n++;
+        for (int col = 0; col < dims.width; col += WIDTH, colData++) {
+            const batch_t* data = colData;
+            int* back[WIDTH];
+            int n[WIDTH];
+
+            for (int w = 0; w < WIDTH; w++) {
+                back[w] = front[w];
+
+                // add top of image
+                *back[w] = -pad;
+                n[w] = 1;
+            }
 
             // must start with a black segment
             if (*data != 0) {
-                *++back = 0;
-                n++;
+                for (int w = 0; w < WIDTH; w++) {
+                    if (((const uint8_t*)(data))[w] != 0) {
+                        *++(back[w]) = 0;
+                        n[w]++;
+                    }
+                }
             }
             data += skip;
 
             // store indices of changes
             for (int row = 1; row < dims.height; row++, data += skip) {
-                if (*data != *(data - skip)) {
-                    if ((n & 1) == 0 && (row - *back) < minHeight) {
-                        // remove noise
-                        back--;
-                        n--;
-                        mNoise++;
-                    } else {
-                        *++back = row;
-                        n++;
+                const batch_t* prev = data - skip;
+                if (*data != *prev) {
+                    for (int w = 0; w < WIDTH; w++) {
+                        if (((const uint8_t*)(data))[w] != ((const uint8_t*)(prev))[w]) {
+                            if ((n[w] & 1) == 0 && (row - *(back[w])) < minHeight) {
+                                // remove noise
+                                back[w]--;
+                                n[w]--;
+                                mNoise++;
+                            } else {
+                                *++(back[w]) = row;
+                                n[w]++;
+                            }
+                        }
                     }
                 }
             }
 
-            // must end with a black segment
-            if ((n & 1) == 0) {
-                *++back = dims.height;
-                n++;
-            }
+            for (int w = 0; w < WIDTH; w++, origX += int16_t(step)) {
+                // must end with a black segment
+                if ((n[w] & 1) == 0) {
+                    *++(back[w]) = dims.height;
+                    n[w]++;
+                }
 
-            // add bottom of image
-            *++back = dims.height + pad;
-            n++;
+                // add bottom of image
+                *++(back[w]) = dims.height + pad;
+                n[w]++;
 
-            // report white segments as strips if all conditions are met
-            int* lastWhite = back - 1;
-            for (int* i = front; i < lastWhite; i += 2) {
-                if (*(i + 1) - *(i + 0) >= minGap && *(i + 3) - *(i + 2) >= minGap) {
-                    int halfHeight = (*(i + 2) - *(i + 1)) * halfStep;
-                    int origY = (*(i + 2) + *(i + 1)) * halfStep;
-                    Pos16 center{int16_t(origX), int16_t(origY)};
-                    Dims16 halfDims{int16_t(halfStep), int16_t(halfHeight)};
-                    cb(center, halfDims);
+                // report white segments as strips if all conditions are met
+                int* lastWhite = back[w] - 1;
+                for (int* i = front[w]; i < lastWhite; i += 2) {
+                    if (*(i + 1) - *(i + 0) >= minGap && *(i + 3) - *(i + 2) >= minGap) {
+                        int halfHeight = (*(i + 2) - *(i + 1)) * halfStep;
+                        int origY = (*(i + 2) + *(i + 1)) * halfStep;
+                        Pos16 center{int16_t(origX), int16_t(origY)};
+                        Dims16 halfDims{int16_t(halfStep), int16_t(halfHeight)};
+                        cb(center, halfDims);
+                    }
                 }
             }
         }
