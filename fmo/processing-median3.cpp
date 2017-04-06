9c326ee9d476af054bfd088422a2153f8bfe2da0
Aleš Hrabalík <tufak064@gmail.com>
2017-04-06 16:51:32 +0200

    Put median3() implementation into a separate file

--- fmo/processing-median3.cpp
+++ fmo/processing-median3.cpp
@@ -3,30 +3,6 @@
 #include <fmo/processing.hpp>
 
 namespace fmo {
-    void decimate(const Mat& src, Mat& dst) {
-        if (src.format() == Format::YUV420SP) {
-            throw std::runtime_error("downscale: source cannot be YUV420SP");
-        }
-
-        Dims srcDims = src.dims();
-        Dims dstDims = {srcDims.width / 2, srcDims.height / 2};
-
-        if (dstDims.width == 0 || dstDims.height == 0) {
-            throw std::runtime_error("downscale: source is too small");
-        }
-
-        dst.resize(src.format(), dstDims);
-        cv::Mat srcMat = src.wrap();
-        cv::Mat dstMat = dst.wrap();
-
-        if (srcDims.width % 2 != 0) { srcMat.flags &= ~cv::Mat::CONTINUOUS_FLAG; }
-
-        srcMat.cols &= ~1;
-        srcMat.rows &= ~1;
-
-        cv::resize(srcMat, dstMat, cv::Size(dstDims.width, dstDims.height), 0, 0, cv::INTER_AREA);
-    }
-
     struct Median3Job : public cv::ParallelLoopBody {
 #if defined(FMO_HAVE_AVX2)
         using batch_t = __m256i;
