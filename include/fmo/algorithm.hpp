5a26dffb77f8508272a15be8eb5fbd289e0b5fa5
Aleš Hrabalík <tufak064@gmail.com>
2017-04-21 17:22:13 +0200

    Add a new output interface

--- include/fmo/algorithm.hpp
+++ include/fmo/algorithm.hpp
@@ -19,9 +19,13 @@ namespace fmo {
         Algorithm& operator=(const Algorithm&) = delete;
         virtual ~Algorithm() {}
         struct Config;
+        struct Detection;
 
         /// Type of a function that produces an Algorithm instance.
         using Factory = std::function<std::unique_ptr<Algorithm>(const Config&, Format, Dims)>;
+
+        /// Type of result produced by an algorithm instance.
+        using Output = std::vector<std::unique_ptr<Detection>>;
 
         /// Configuration settings determining the properties of a new Algorithm instance. Pass the
         /// object into the make() static method.
@@ -140,6 +144,42 @@ namespace fmo {
             Image temp3;     ///< object for storing auxiliary image data
         };
 
+        /// A structure that contains all relevant information about a detected object. The user of
+        /// the algorithm receives subclasses of this structure using the getOutput() method.
+        /// Instances of this class are invalidated by the next call to setInputSwap().
+        struct Detection {
+            virtual ~Detection() = default;
+
+            /// Constructor variant with the previous center available.
+            Detection(Pos center, Pos prevCenter, float radius)
+                : mCenter(center), mPrevCenter(prevCenter), mRadius(radius) {}
+
+            /// Constructor variant with the previous center unavailable.
+            Detection(Pos center, float radius)
+                : mCenter(center), mPrevCenter{-1, -1}, mRadius(radius) {}
+
+            /// Midpoint of the object in the current frame.
+            Pos getCenter() const { return mCenter; }
+
+            /// Midpoint of the object in the previous frame. The value might not be available; use
+            /// havePrevCenter() to check.
+            Pos getPrevCenter() const { return mPrevCenter; }
+
+            /// Checks whether getPrevCenter() may be called.
+            bool havePrevCenter() const { return mPrevCenter.x >= 0; }
+
+            /// Apparent radius of the object in pixels.
+            float getRadius() const { return mRadius; }
+
+            /// Generates coordinates of object pixels and sorts them according to pointSetCompLt().
+            virtual void getPoints(PointSet& out) const = 0;
+
+        private:
+            Pos mCenter;
+            Pos mPrevCenter;
+            float mRadius;
+        };
+
         /// Creates a new instance of an Algorithm. The field config.name is used to determine which
         /// algorithm factory will be used. The factory must have been previously added with the
         /// registerFactory() static method.
@@ -157,6 +197,11 @@ namespace fmo {
         /// take place during the call and might take a long time. The input is received by swapping
         /// the contents of the provided input image with an internal buffer.
         virtual void setInputSwap(Image& input) = 0;
+
+        /// To be called every frame, obtaining a list of fast-moving objects that have been
+        /// detected this frame. The returned objects (i.e. instances of class Detection) may be
+        /// used only before the next call to setInputSwap().
+        virtual void getOutput(Output& output) { output.clear(); }
 
         /// Visualizes the result of detection, returning an image that is useful for debugging
         /// algorithm behavior. The returned image will have BGR format and the same dimensions as
